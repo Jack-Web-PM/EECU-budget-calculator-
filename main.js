7bb814e7f40bfbf6dc4d45144d5dde3a70f8b13f
Jack-Web-PM <jack.chou24@student.cart.org>
2025-03-17 15:41:06 -0700

    update

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ const chartBtns = document.querySelectorAll(".changeChart > button");
 new Chart(pieChartCanvas, {
   type: 'doughnut',
   data: {
-    labels: ['Blue', 'Red', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['/12 Monthly Gross', '12% State Tax', '7% Fed Tax', '6.2% Social Security', '1.45% Medicare', '1% State Disability', '5% Retirement Investment', '-$180 Medical Insurance', 'House Payment Afford 33%', 'Your Monthly Income'],
     datasets: [{
       label: 'cost of #',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 12, 7, 5, 2, 3, 3, 3, 3, 3],
       borderWidth: 1
     }]
   },
@@ -33,7 +33,7 @@ new Chart(pieChartCanvas, {
 new Chart(barChartCanvas, {
   type: 'bar',
   data: {
-    labels: ['Blue', 'Red', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Blue', 'Red', 'Yellow', 'Green', 'Purple', 'Orange',],
     datasets: [{
       label: 'cost of #',
       data: [12, 19, 3, 5, 2, 3],
@@ -107,3 +107,67 @@ function light() {
 //     document.body.classList.add("light-mode");
 //   }
 // });
+
+
+function reset(){
+  let table = document.getElementById("check");
+  table.innerHTML = `<tr>
+  <td class="regTable">
+      <input type="text" placeholder="Title" id="21">
+  </td>
+  <td class="regTable">
+      <button onclick="swapDW()" id="22">Deposit</button>
+  </td>
+  <td class="regTable">
+      <input type="text" placeholder="Amount" id="23">
+  </td>
+  <td class="regTable">
+      <button onclick="addPayment()">Add Payment</button>
+  </td>
+</tr>`
+
+  k = b - j;
+  document.getElementById("ahhhh").innerText = "Your current Balance is $" + k.toFixed(2);
+}
+
+function addPayment () {
+  let table = document.getElementById("check");
+  let row = document.createElement("tr")
+  let first = document.createElement("td")
+  let second = document.createElement("td")
+  let third = document.createElement("td")
+  let fourth = document.createElement("td")
+
+  let name = document.getElementById("21").value;
+  first.innerText = name;
+  
+  let amount = parseFloat(document.getElementById("23").value);
+  third.innerText = "$" + amount;
+
+  let m;
+  
+  if (dwBool) {
+      second.innerText = "Deposit";
+      m = k + amount;
+  } else {
+      second.innerText = "Withdrawal";
+      m = k - amount;
+  }
+
+  fourth.innerText = "$" + m.toFixed(2);
+  k = m;
+
+  first.setAttribute("class", "cool2");
+  second.setAttribute("class", "cool2");
+  third.setAttribute("class", "cool2");
+  fourth.setAttribute("class", "cool2");
+  row.append(first);
+  row.append(second);
+  row.append(third);
+  row.append(fourth);
+  table.append(row);
+
+}
+
+
+
